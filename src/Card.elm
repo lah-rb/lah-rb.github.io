17a6af3a56b92c1064dac56a5486784969be6741
Luke Hayes <luke.a.hayes@outlook.com>
2020-09-15 14:21:44 -0600

    Add card polish

--- src/Card.elm
+++ src/Card.elm
@@ -15,9 +15,9 @@ cardModel =
 
 view card =
   div[class "flex content-start flex-col"][
-      h1 [class "sm:text-yellow-600 font-semibold font-serif text-center text-2xl md:text-5xl"][text cardModel.name]
-    , img[class "sm:p-5 mx-auto md:w-1/4 h-1/4", src "pics/sunflower_center.png"][]
-    , ul [class "sm:p-5 leading-10 mx-auto text-left md:w-1/4"][
+      h1 [class "sm:text-yellow-600 font-semibold font-serif text-center text-2xl lg:text-5xl"][text cardModel.name]
+    , img[class "sm:p-5 mx-auto lg:w-1/4 h-1/4", src "pics/sunflower_center.png"][]
+    , ul [class "sm:p-5 leading-10 mx-auto text-left lg:w-1/4"][
          li[class "leading-snug p-2"][span[class "text-orange-400"][text "Scarcity: "], span[class "text-gray-700"][text cardModel.scarcity]]
       ,  li[class "leading-snug p-2"][span[class "text-orange-900"][text "History: "], p[class "text-gray-700"][text cardModel.description]]
       ,  li[class "leading-snug p-2"][span[class "text-pink-400"][text "Play Style: "], p[class "text-gray-700"][text cardModel.play]]
