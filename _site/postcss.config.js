b25e753e07873920b90194b8aef417cf026b0b71
Luke Hayes <luke.a.hayes@outlook.com>
2020-09-22 18:48:15 -0600

    Initial search implementation

--- _site/postcss.config.js
+++ _site/postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+      content: [
+        './404.html',
+        './index.html',
+        './_layouts/**.html',
+        './_posts/**.html',
+        './_site/**.html'
+      ],
+      defaultExtractor: content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
   ]
 }
