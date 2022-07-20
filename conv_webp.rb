47f27c19945c76b836497628254d4b8cd3dc7cf7
Luke Hayes <luke.a.hayes@outlook.com>
2022-07-20 12:36:59 -0600

    Add motive mechanic to site

--- conv_webp.rb
+++ conv_webp.rb
@@ -3,7 +3,7 @@ include FileUtils
 
 full_site_images = Dir['assets/images/*']
 thumbnail_images = Dir['assets/thumbnails/*']
-png_site_images = full_site_images + thumbnail_images
+png_site_images = full_site_images #+ thumbnail_images
 png_site_images.keep_if { |image| image.split('.')[1] == 'png' }
 png_site_images.each do |image|
   path_without_suffix = image.split('.')
