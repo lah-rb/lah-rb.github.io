b4f5c59492a72b94b8800bcb489860d97e390b44
Luke Hayes <luke.a.hayes@outlook.com>
2024-06-05 12:19:48 -0600

    Add filter functionallity and use workbox cli to generate service worker

---
+++ workbox-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	globDirectory: '_site/',
+	globPatterns: [
+		'**/*.{html,png,css,webp,js,wasm,svg,xml,ico,pdf,json,webmanifest}'
+	],
+	swDest: './sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	],
+	runtimeCaching: [{
+		urlPattern: /https:\/\/www.kipukas.cards/,
+		handler: 'StaleWhileRevalidate'
+		}]
+};
