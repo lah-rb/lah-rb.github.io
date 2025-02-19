b055575ee9798b639345d7d0ba19bd656dd10965
Luke Hayes <luke.a.hayes@outlook.com>
2025-02-18 22:26:24 -0700

    try versioning for workbox

---
+++ update-version.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'digest/sha2'
+require 'find'
+
+def dir_sha256(directory)
+  # Initialize the hash
+  sha256 = Digest::SHA256.new
+
+  # Iterate over all files in the directory and update the hash
+  Find.find(directory) do |path|
+    next if File.directory?(path) # Skip directories themselves
+    sha256.update(File.read(path))
+  end
+
+  sha256.hexdigest
+end
+
+directory = '/home/lah-rb/Repos/lah-rb.github.io'
+new_version = dir_sha256(directory) + Time.now.strftime("%Y-%m-%d_%H-%M-%S")
+
+# Write the new version to version.txt
+File.open("#{directory}/version.txt", 'w') { |f| f.puts new_version }
+
+puts "New version generated: #{new_version}"
--- workbox-config.js
+++ workbox-config.js
@@ -1,15 +1,24 @@
+const fs = require('fs');
+const versionHash = fs.readFileSync('./version.txt', 'utf8').trim();
+
 module.exports = {
-	globDirectory: '_site/',
-	globPatterns: [
-		'**/*.{html,png,css,webp,js,wasm,svg,yml,ico,pdf,json,webmanifest}'
-	],
-	swDest: './sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	],
-	runtimeCaching: [{
-		urlPattern: /https:\/\/www.kipukas.cards/,
-		handler: 'StaleWhileRevalidate'
-		}],
+  globDirectory: '_site/',
+  globPatterns: [
+    '**/*.{html,png,css,webp,js,wasm,svg,yml,ico,pdf,json,webmanifest}'
+  ],
+  swDest: './sw.js',
+  ignoreURLParametersMatching: [
+    /^utm_/,
+    /^fbclid$/
+  ],
+  runtimeCaching: [{
+    urlPattern: /https?:\/\/[^\/]+/,
+    handler: 'StaleWhileRevalidate'
+  }],
+  clientsClaim: true,
+  skipWaiting: true,
+  // Use the versionHash in a way that Workbox understands (e.g., as part of the cache names)
+  cacheId: 'my-app-cache-' + versionHash,
 };
+
+  
