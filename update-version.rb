0a3b6cc2171ff31588af4b22c02d778709a2711c
Luke Hayes <luke.a.hayes@outlook.com>
2025-02-19 09:20:06 -0700

    Fix no swipe on mobile

--- update-version.rb
+++ update-version.rb
@@ -16,7 +16,7 @@ def dir_sha256(directory)
   sha256.hexdigest
 end
 
-directory = '/home/lah-rb/Repos/lah-rb.github.io'
+directory = '/home/lah-rb/Repos/lah-rb.github.io/_site'
 new_version = dir_sha256(directory) + Time.now.strftime("%Y-%m-%d_%H-%M-%S")
 
 # Write the new version to version.txt
